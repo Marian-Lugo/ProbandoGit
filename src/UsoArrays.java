7af158021f0cccc454d33a49958340c5a16dfd43
marian <mnelugo@gmail.com>
2021-05-22 17:22:24 -0400

    Agregue un print

---
+++ src/UsoArrays.java
@@ -0,0 +1,21 @@
+
+public class UsoArrays {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	   int mi_Array[] = new int[5];
+	   int a=4;
+	   for (int i = 0; i < mi_Array.length; i++) 
+	   {
+		   mi_Array[i]=a--; 
+		   System.out.println("Posicion="+i+"Valor="+mi_Array[i]);
+	   }
+	   
+	   for (int i = 0; i < mi_Array.length; i++) {
+		
+		   System.out.println("esta es una prueba para git");
+	   }
+	}
+
+}
