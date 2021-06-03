39498e6df66d1128eab31881fcb966efc7ec9a97
marian <mnelugo@gmail.com>
2021-06-02 21:17:35 -0400

    Probando git con eclipse

--- src/CreandoUnArchivoDesdeGitHub.java
+++ src/CreandoUnArchivoDesdeGitHub.java
@@ -1,5 +1,9 @@
+public class CreandoUnArchivoDesdeGitHub{
+
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Probando GitHub Rama secundaria");
+		System.out.println("Probando Git en eclipse");
 
 	}
+}
